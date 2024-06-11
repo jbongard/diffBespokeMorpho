dadc95b890ef862ee7e0b253bc6a33784bd2f394
jbongard <jbongard@.uvm.edu>
2024-06-11 11:17:29 -0400

    merging vertically now.

--- mergeFrames.py
+++ mergeFrames.py
@@ -5,13 +5,13 @@ for frame in range(170):
    image1 = Image.open("mass_spring/initial4/"+str(frame)+".png")
    image2 = Image.open("mass_spring/final4/"+str(frame)+".png")
 
-   width = image1.width + image2.width
-   height  = max(image1.height, image2.height)
+   height = image1.height + image2.height
+   width  = max(image1.width, image2.width)
 
    combined_image = Image.new('RGBA', (width, height))
 
    combined_image.paste(image1, (0, 0))
-   combined_image.paste(image2, (image1.width, 0))
+   combined_image.paste(image2, (0, image1.height))
 
    combined_image.save("mass_spring/merged4/"+str(frame)+".png")
    #combined_image.show()
