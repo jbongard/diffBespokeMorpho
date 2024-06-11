1c31c84cab414691a777cccab87bec6f0b93aa1a
jbongard <jbongard@.uvm.edu>
2024-06-11 11:32:24 -0400

    updated colab.py

--- colab.py
+++ colab.py
@@ -3,4 +3,12 @@ import os
 os.system("mkdir   mass_spring")
 os.system("mkdir   mass_spring/initial4")
 os.system("mkdir   mass_spring/final4")
-os.system("python3 diffBespokeMorpho/mass_spring_bodyChange.py 4 train 50")
+os.system("python3 diffBespokeMorpho/mass_spring_bodyChange.py 4 train 0")
+
+os.system("!mkdir mass_spring/merged4")
+os.system("!python3 diffBespokeMorpho/mergeFrames.py")
+os.system("!rm -r mass_spring/initial4")
+os.system("!rm -r mass_spring/final4")
+os.system("!rm beforeAfter.mp4")
+os.system("!ffmpeg -i mass_spring/merged4/%d.png -c:v libx264 -pix_fmt yuv420p beforeAfter.mp4")
+os.system("!rm -r mass_spring/merged4")
